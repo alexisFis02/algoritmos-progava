57fa1adcd8e1129b8f96d5be242150c347568ab0
lclivio <lclivio@unlam.edu.ar>
2024-05-16 15:26:30 -0300

    Interface para sorting.

--- src/main/java/org/prograva/sorting/Ordenables.java
+++ src/main/java/org/prograva/sorting/Ordenables.java
@@ -1,5 +1,10 @@
 package org.prograva.sorting;
 
 public interface Ordenables {
-	
+	void quickSort(int[] array);
+	void shellSort(int[] array);
+	void mergeSort(int[] array);
+	void selectionSort(int[] array);
+	void insertionSort(int[] array);
+	void bubbleSort(int[] array);
 }
