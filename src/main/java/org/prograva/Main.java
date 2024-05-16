2cdf0ce832c7d5a0e62928f5c578a2f9d9e166c9
lclivio <lclivio@unlam.edu.ar>
2024-05-16 15:33:34 -0300

    Refactor Ordenamientos uso de interface.

--- src/main/java/org/prograva/Main.java
+++ src/main/java/org/prograva/Main.java
@@ -6,12 +6,6 @@ import org.prograva.sorting.AlgoritmosOrdenamiento;
 
 public class Main {
     public static void main(String[] args) {
-    	
-        System.out.println(testearAlgoritmo(1000,1000,AlgoritmosOrdenamiento::shellSort));
-        System.out.println(testearAlgoritmo(1000,1000,AlgoritmosOrdenamiento::selectionSort));
-        System.out.println(testearAlgoritmo(1000,1000,AlgoritmosOrdenamiento::insertionSort));
-        System.out.println(testearAlgoritmo(1000,1000,AlgoritmosOrdenamiento::mergeSort));
-        System.out.println(testearAlgoritmo(1000,1000,AlgoritmosOrdenamiento::quickSort));
     }
 
 }
--- src/main/java/org/prograva/sorting/AlgoritmosOrdenamiento.java
+++ src/main/java/org/prograva/sorting/AlgoritmosOrdenamiento.java
@@ -2,17 +2,17 @@ package org.prograva.sorting;
 
 import java.util.*;
 
-public class AlgoritmosOrdenamiento {
+public class AlgoritmosOrdenamiento implements Ordenables{
 
 
-    public static void quickSort(int[]a, int n) {
-        quickSortImp(a,0,n-1);
+    public void quickSort(int[]a) {
+        quickSortImp(a,0,a.length);
     }
 
     /*
     * Implementacion de quick sort con pivot en el medio
     */
-    public static void quickSortImp(int[] a, int inf,int sup) {
+    private static void quickSortImp(int[] a, int inf,int sup) {
 
         if(sup - inf <= 1) return;
         int pivot = a[((sup-inf)/2) + inf];
@@ -44,7 +44,8 @@ public class AlgoritmosOrdenamiento {
     /*
     * Insersion interativa sobre elementos que distan un valor de la serie de shell
     * */
-    public static void shellSort(int[]a, int n){
+    public void shellSort(int[]a){
+    	int n = a.length;
         int[] serieShell = getSerieShell(n);
         int i,j,k,act,shell,elementos,l;
 
@@ -71,7 +72,7 @@ public class AlgoritmosOrdenamiento {
         }
     }
 
-    public static int[] getSerieShell(int n){
+    private static int[] getSerieShell(int n){
         int[] ret = new int[n];
         ret[0] = 1;
         int i = 0;
@@ -86,14 +87,14 @@ public class AlgoritmosOrdenamiento {
     * Wrapper del merge sort
     * Por que la implementacion require devolver int[]
     * */
-    public static void mergeSort(int[]a, int n){
-        System.arraycopy(mergeSortImp(a,n),0,a,0,n);
+    public void mergeSort(int[]a){
+        System.arraycopy(mergeSortImp(a,a.length),0,a,0,a.length);
     }
 
     /*
     * merge sort
     * */
-    public static int[] mergeSortImp(int[] a, int n) {
+    private static int[] mergeSortImp(int[] a, int n) {
         if(n==1){
             return a;
         }
@@ -111,7 +112,7 @@ public class AlgoritmosOrdenamiento {
     /*
     * Fuciona los dos arrays ordenados en un unico array ordenado
     * */
-    public static int[] merge(int[] der,int lenDer, int[]izq , int lenIzq) {
+    private static int[] merge(int[] der,int lenDer, int[]izq , int lenIzq) {
         int[] ret = new int[ lenIzq + lenDer];
 
         int i = 0,j = 0,k = 0;
@@ -129,7 +130,8 @@ public class AlgoritmosOrdenamiento {
     * Ordenamiento por seleccion
     * recorre el array buscando e intercambiando el menor elemento en la parte izquierda del array desde el actual
     * */
-    public static void selectionSort(int[] a, int n) {
+    public void selectionSort(int[] a) {
+    	int n = a.length;
         int iact, j, imenor;
         int act;
         for (iact = 0; iact < n; iact++) {
@@ -146,10 +148,10 @@ public class AlgoritmosOrdenamiento {
     * recorre el array comparando el actual con cada uno de los anteriores desplanzandolos hacia adelante
     * cuando encuenta uno menos al actual en el anteriores lo inserta
     * */
-    public static void insertionSort(int[] a, int n) {
+    public void insertionSort(int[] a) {
         int i, j;
         int act;
-        for (i = 1; i < n; i++) {
+        for (i = 1; i < a.length; i++) {
             act = a[i];
             for (j = i; j >= 0; j--) { // itero desde el indice del actual hasta el inicio
                 if (j == 0 || act > a[j - 1]) { // si el actual es mayor al anterior lo inserto
@@ -162,7 +164,7 @@ public class AlgoritmosOrdenamiento {
         }
     }
 
-    public static void bubbleSort(int[] a, int[] n) {
+    public void bubbleSort(int[] a) {
         // TODO: completar
     }
 
@@ -174,7 +176,7 @@ public class AlgoritmosOrdenamiento {
      * @param algoritmo      La interfaz que define el algoritmo de ordenamiento a probar.
      * @return               true si todas las pruebas pasan satisfactoriamente, false en caso contrario.
      */
-    public static boolean testearAlgoritmo(int n, int maxElements, AlgoritmosOrdenamientoInterface algoritmo) {
+    private static boolean testearAlgoritmo(int n, int maxElements) {
         Random random = new Random();
         for (int i = 0; i < n; i++) {
             int elementos = random.nextInt(maxElements) + 1;
@@ -182,7 +184,7 @@ public class AlgoritmosOrdenamiento {
             int[] original = generarArrayAleatorio(elementos);
             int[] aOrdenar = original.clone();
 
-            algoritmo.sort(aOrdenar, elementos);
+            //algoritmo.sort(aOrdenar, elementos);
 
             if (!verificarOrden(aOrdenar,original)){
                 System.out.println("FALLO");
@@ -194,7 +196,7 @@ public class AlgoritmosOrdenamiento {
         return true;
     }
 
-    public static int[] generarArrayAleatorio(int longitud) {
+    private static int[] generarArrayAleatorio(int longitud) {
         Random random = new Random();
         int[] array = new int[longitud];
         for (int i = 0; i < longitud; i++) {
@@ -203,7 +205,7 @@ public class AlgoritmosOrdenamiento {
         return array;
     }
 
-    public static boolean verificarOrden(int[] ordenado, int[] original) {
+    private static boolean verificarOrden(int[] ordenado, int[] original) {
         if (ordenado == null || original == null || original.length != ordenado.length) return false;
         Arrays.sort(original);
         return Arrays.equals(ordenado, original);
@@ -212,13 +214,9 @@ public class AlgoritmosOrdenamiento {
     /*
     * Intercambia el valor en la posicion i con el valor de la posicion j en el array
     * */
-    public static void swap(int[] array, int i, int j) {
+    private static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-    }
-
-    public interface AlgoritmosOrdenamientoInterface {
-        void sort(int[] array,int n);
     }
 }
