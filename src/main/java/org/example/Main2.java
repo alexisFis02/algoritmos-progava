38a5070877396fb027fd306a3bc6b66534c9dd34
Ritomii <matiasagusrosano@gmail.com>
2024-06-24 00:33:59 -0300

    Agrupa algoritmos de grafos y añade algoritmo de Djikstra

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -2,8 +2,7 @@ package org.example;
 
 //import java.util.Arrays;
 
-import static org.example.BFS.*;
-import static org.example.DFS.*;
+import static org.example.AlgoritmosGrafos.*;
 import java.util.*;
 
 public class Main2 {
@@ -20,9 +19,18 @@ public class Main2 {
         
         System.out.println("Breadth-First Search desde el nodo 2:");
         Map<Integer, Integer> distancias = algoritmoBFS(grafo, 2);
-        for (Map.Entry<Integer, Integer> entry : distancias.entrySet()) {
-            System.out.println("Nodo " + entry.getKey() + " - Distancia: " + entry.getValue());
+        for (Map.Entry<Integer, Integer> entrada : distancias.entrySet()) {
+            System.out.println("Nodo " + entrada.getKey() + " - Distancia: " + entrada.getValue());
         }
-    }
+        
+        Map<Integer, List<NodoPeso>> grafoPeso = new HashMap<>();
+        grafoPeso.put(0, Arrays.asList(new NodoPeso(1, 4), new NodoPeso(2, 1)));
+        grafoPeso.put(1, Arrays.asList(new NodoPeso(3, 1)));
+        grafoPeso.put(2, Arrays.asList(new NodoPeso(1, 2), new NodoPeso(3, 5)));
+        grafoPeso.put(3, Arrays.asList());
 
+        algoritmoDjikstra(grafoPeso, 0);
+
+        
+    	}
 }
---
+++ src/main/java/org/example/NodoPeso.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class NodoPeso {
+    int nodo;
+    int peso;
+
+    public NodoPeso(int nodo, int peso) {
+        this.nodo = nodo;
+        this.peso = peso;
+    }
+}
